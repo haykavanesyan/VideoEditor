71f23448387d654adca5e5e157a4026443cc7f4b
haykavanesyan <haykavanesyan09@gmail.com>
2025-11-08 00:11:41 +0400

    fix SharedArrayBuffer error

--- src/services/errorService.ts
+++ src/services/errorService.ts
@@ -2,6 +2,7 @@ import { toast } from 'react-toastify';
 
 export const errorService = {
     handle(type: 'error' | 'warning', error: unknown, message?: string) {
+        console.error(error, message)
         const text =
             message ||
             (error instanceof Error ? error.message : 'Unexpected error occurred');
